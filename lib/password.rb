4588e4449a577e47cbd5702b1e637bc781fe4b14
chmeee <chmeee@gmail.com>
2009-07-10 22:21:34 +0200

    Stats output funcional

--- lib/password.rb
+++ lib/password.rb
@@ -2,26 +2,27 @@ module PwdumpAnalysis
 
 class Password
   def initialize(line)
-    @uid     = ""
-    @lm_hash = ""
-    @nt_hash = ""
     @lm_pwd  = Array.new
-    @nt_pwd  = ""
 
-    @uid, id, @lm_hash, @nt_hash, @lm_pwd[0], @lm_pwd[1], @nt_pwd = line.split(':')
-    @nt_pwd.chomp!
+    @uid, @id, @lm_hash, @nt_hash, @lm_pwd[0], @lm_pwd[1], @nt_pwd = line.split(':')
+
+    if @nt_pwd.nil?
+      @nt_pwd = ""
+    else
+      @nt_pwd.chomp!
+    end
   end
 
   def is_alpha?
-    @nt_pwd ~ /[a-zñA-ZÑ]+/
+    @nt_pwd =~ /^[a-zñA-ZÑ]+$/
   end
 
   def is_num?
-    @nt_pwd ~ /[0-9]+/
+    @nt_pwd =~ /^[0-9]+$/
   end
 
   def is_alphanum?
-    @nt_pwd ~ /[a-zñA-ZÑ0-9]+/
+    @nt_pwd =~ /[a-zñA-ZÑ]+/ and @nt_pwd =~ /[0-9]+/ and @nt_pwd !~ /[^a-zñA-ZÑ0-9]+/
   end
 
   def pwd_length
@@ -34,6 +35,10 @@ class Password
 
   def decrypted?
     ! @nt_pwd.empty?
+  end
+
+  def same_id?
+    @uid == @nt_pwd
   end
 
 end
--- lib/stats.rb
+++ lib/stats.rb
@@ -2,6 +2,8 @@ module PwdumpAnalysis
 
 class Stats
   def initialize(input_file, output_dir)
+    @pwds = Array.new
+
     @pwd_max_length = 0
     @pwd_min_length = 0
     @uid_max_length = 0
@@ -10,7 +12,11 @@ class Stats
     @alpha_cnt      = 0
     @num_cnt        = 0
     @alphanum_cnt   = 0
-    @decrypted_cnt  = 0
+    @same_id_cnt    = 0
+    @found_cnt      = 0
+
+    @pwd_hist = Hash.new
+    @uid_hist = Hash.new
 
     @input_file = input_file
     @output_dir = output_dir
@@ -19,9 +25,18 @@ class Stats
   end
 
   def scan_file
-    open(@input_file) do |f|
-      f.each do |line|
+      @input_file.each do |line|
         pass = Password.new(line)
+
+        @pwds << pass
+
+        @pwd_hist[pass.pwd_length] = (@pwd_hist[pass.pwd_length].nil?) ? 1 : @pwd_hist[pass.pwd_length] + 1
+        @uid_hist[pass.uid_length] = (@uid_hist[pass.uid_length].nil?) ? 1 : @uid_hist[pass.uid_length] + 1
+
+        @alpha_cnt      += 1 if pass.is_alpha?
+        @num_cnt        += 1 if pass.is_num?
+        @alphanum_cnt   += 1 if pass.is_alphanum?
+        @same_id_cnt    += 1 if pass.same_id?
 
         @pwd_max_length = (pass.pwd_length > @pwd_max_length) ? pass.pwd_length : @pwd_max_length
         @pwd_min_length = (pass.pwd_length < @pwd_min_length) ? pass.pwd_length : @pwd_min_length
@@ -30,20 +45,39 @@ class Stats
 
         @found_cnt += 1 if pass.decrypted?
       end
-    end
 
   end
 
   def print_stats
+    puts "-------[ PWDUMP analysis ]-------"
+    print_one_stat "Found",       @found_cnt
+    print_one_stat "Not found",   @pwds.length - @found_cnt
+    print_one_stat "Total",       @pwds.length
+    puts "---------------------------------"
+    print_one_stat "id == pwd",   @same_id_cnt
+    print_one_stat "id != pwd",   @pwds.length - @same_id_cnt
+    puts "---------------------------------"
+    print_one_stat "Alphabetic",  @alpha_cnt
+    print_one_stat "Numeric",     @num_cnt
+    print_one_stat "Alphanum",    @alphanum_cnt
+    print_one_stat "Other",      @pwds.length - (@alpha_cnt + @num_cnt + @alphanum_cnt)
+    puts "---------------------------------"
+  end
 
+  def print_one_stat(text, value)
+    printf " %-15s %5d\t%6.2f%%\n", text+":", value, value * 100.0 / @pwds.length
   end
 
   def generate_graphs 
+    print "generating graphs..."
 
+    puts  "[DONE]"
   end
 
   def generate_csv 
+    print "generating graphs..."
 
+    puts  "[DONE]"
   end
 end
 
