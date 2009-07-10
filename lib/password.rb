1e88d4e6d12489e541c7b2d4b40d337dd3734e94
chmeee <chmeee@gmail.com>
2009-07-10 09:14:40 +0200

    Added CSV output option Completed Stats.scan_file method Added app logic to main file Added decrypted? method to class Password

--- lib/password.rb
+++ lib/password.rb
@@ -9,27 +9,31 @@ class Password
     @nt_pwd  = ""
 
     @uid, id, @lm_hash, @nt_hash, @lm_pwd[0], @lm_pwd[1], @nt_pwd = line.split(':')
+    @nt_pwd.chomp!
   end
 
   def is_alpha?
-  # nt_pwd
-
+    @nt_pwd ~ /[a-zñA-ZÑ]+/
   end
 
   def is_num?
-
+    @nt_pwd ~ /[0-9]+/
   end
 
   def is_alphanum?
-
+    @nt_pwd ~ /[a-zñA-ZÑ0-9]+/
   end
 
   def pwd_length
-
+    @nt_pwd.length
   end
 
   def uid_length
+    @uid.length
+  end
 
+  def decrypted?
+    ! @nt_pwd.empty?
   end
 
 end
--- lib/stats.rb
+++ lib/stats.rb
@@ -7,18 +7,30 @@ class Stats
     @uid_max_length = 0
     @uid_min_length = 0
 
-    @alpha_count    = 0
-    @num_count      = 0
-    @alphanum_count = 0
+    @alpha_cnt      = 0
+    @num_cnt        = 0
+    @alphanum_cnt   = 0
+    @decrypted_cnt  = 0
 
     @input_file = input_file
     @output_dir = output_dir
 
-    scan_file(input_file)
+    scan_file
   end
 
   def scan_file
-    
+    open(@input_file) do |f|
+      f.each do |line|
+        pass = Password.new(line)
+
+        @pwd_max_length = (pass.pwd_length > @pwd_max_length) ? pass.pwd_length : @pwd_max_length
+        @pwd_min_length = (pass.pwd_length < @pwd_min_length) ? pass.pwd_length : @pwd_min_length
+        @uid_max_length = (pass.uid_length > @uid_max_length) ? pass.uid_length : @uid_max_length
+        @uid_min_length = (pass.uid_length < @uid_min_length) ? pass.uid_length : @uid_min_length
+
+        @found_cnt += 1 if pass.decrypted?
+      end
+    end
 
   end
 
@@ -26,7 +38,11 @@ class Stats
 
   end
 
-  def create_graphs 
+  def generate_graphs 
+
+  end
+
+  def generate_csv 
 
   end
 end
